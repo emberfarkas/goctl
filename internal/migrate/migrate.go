674081fde99876016d32c0c3b61d4b7f5c75fa46
emberfarkas <emberfarkas@hotmail.com>
2022-12-10 17:22:16 +0800

    Update migrate.go

--- internal/migrate/migrate.go
+++ internal/migrate/migrate.go
@@ -3,10 +3,13 @@ package migrate
 import (
 	"errors"
 	"fmt"
+	"os"
 	"time"
 
+	"github.com/go-bamboo/pkg/log"
 	"github.com/go-bamboo/pkg/migrate"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -16,18 +19,52 @@ var (
 	errNotSupport               = errors.New("not support cmd")
 )
 
+// DefaultOutPath default path
+const DefaultOutPath = "./migrations"
+
 var (
 	defaultTimeFormat = "20060102150405"
-	name              string
-	dsn               string
-	user              string = "root"
-	pass              string = "123456"
-	host              string = "127.0.0.1"
-	port              uint32 = 3306
-	db                string
-	table             string
-	path              string
+
+	name string
+
+	genPath   string
+	dsn       string
+	db        string
+	tableList string
+	outPath   string
 )
+
+// CmdParams is command line parameters
+type CmdParams struct {
+	DSN     string `yaml:"dsn"`     // consult[https://gorm.io/docs/connecting_to_the_database.html]"
+	DB      string `yaml:"db"`      // input mysql or postgres or sqlite or sqlserver. consult[https://gorm.io/docs/connecting_to_the_database.html]
+	OutPath string `yaml:"outPath"` // specify a directory for output
+}
+
+// YamlConfig is yaml config struct
+type YamlConfig struct {
+	Version  string     `yaml:"version"`  //
+	Database *CmdParams `yaml:"database"` //
+}
+
+type Config struct {
+	GenGorm YamlConfig `yaml:"migrate"`
+}
+
+// loadConfigFile load config file from path
+func loadConfigFile(path string) (*CmdParams, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close() // nolint
+	var config Config
+	if cmdErr := yaml.NewDecoder(file).Decode(&config); cmdErr != nil {
+		return nil, cmdErr
+	}
+	log.Debugf("dsn: %v, db: %v", config.GenGorm.Database.DSN, config.GenGorm.Database.DB)
+	return config.GenGorm.Database, nil
+}
 
 // Cmd represents the new command
 var Cmd = &cobra.Command{
@@ -63,12 +100,27 @@ var upCmd = &cobra.Command{
 	Short: "更新数据库",
 	Long:  `更新数据库`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var cmdParse CmdParams
+		if genPath != "" {
+			if configFileParams, err := loadConfigFile(genPath); err == nil && configFileParams != nil {
+				cmdParse = *configFileParams
+			}
+		}
+		// cmd first
+		if dsn != "" {
+			cmdParse.DSN = dsn
+		}
+		if db != "" {
+			cmdParse.DB = db
+		}
+		if outPath != "" {
+			cmdParse.OutPath = outPath
+		}
 		//
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=utf8&parseTime=true", user, pass, host, port, db)
-		sourceURL := fmt.Sprintf("file://%v", path)
+		sourceURL := fmt.Sprintf("file://%v", cmdParse.OutPath)
 		mf := &migrate.Config{
-			Driver:    "mysql",
-			Source:    dsn,
+			Driver:    cmdParse.DB,
+			Source:    cmdParse.DSN,
 			SourceURL: sourceURL,
 		}
 		if err := migrate.MigrateUp(mf); err != nil {
@@ -83,15 +135,13 @@ func init() {
 
 	// Here you will define your flags and configuration settings.
 
-	newCmd.Flags().StringVarP(&name, "name", "n", "default", "file name")
+	newCmd.Flags().StringVar(&name, "name", "default", "file name")
 
 	exportCmd.Flags().StringVar(&dsn, "dsn", "", "数据库链接")
-	exportCmd.Flags().StringVar(&table, "table", "", "表")
+	exportCmd.Flags().StringVar(&tableList, "tables", "", "表")
 
-	upCmd.Flags().StringVarP(&user, "user", "u", "root", "用户")
-	upCmd.Flags().StringVarP(&pass, "pass", "p", "123456", "密码")
-	upCmd.Flags().StringVar(&host, "host", "127.0.0.1", "主机")
-	upCmd.Flags().Uint32Var(&port, "port", 3306, "端口")
-	upCmd.Flags().StringVar(&db, "db", "", "数据库")
-	upCmd.Flags().StringVar(&path, "path", "./migrations", "路径")
+	upCmd.Flags().StringVar(&genPath, "c", "", "is path for gen.yml")
+	upCmd.Flags().StringVar(&dsn, "dsn", "", "consult[https://gorm.io/docs/connecting_to_the_database.html]")
+	upCmd.Flags().StringVar(&db, "db", "", "input mysql or postgres or sqlite or sqlserver. consult[https://gorm.io/docs/connecting_to_the_database.html]")
+	upCmd.Flags().StringVar(&outPath, "outPath", DefaultOutPath, "specify a directory for output")
 }
