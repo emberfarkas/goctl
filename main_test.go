9280b6ea1216661fb3ab1718f4b94efbe810079e
songhao <emberfarkas@hotmail.com>
2023-04-21 11:31:16 +0800

    feat: workflow

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"unicode"
 	"unicode/utf8"
@@ -12,4 +13,9 @@ func TestXxx(t *testing.T) {
 	if unicode.Is(unicode.Han, r) {
 		t.Logf("汉字")
 	}
+}
+
+func TestInt(t *testing.T) {
+	n := fmt.Sprintf("%.4f", float64(11000)/float64(10000))
+	t.Errorf("%v", n) // 1.1000
 }
