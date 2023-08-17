02200141bca7e776c81338fbf35cacfae233bf6e
songhao <emberfarkas@hotmail.com>
2023-08-17 17:34:38 +0800

    feat: force

---
+++ internal/migrate/force/force.go
@@ -0,0 +1,66 @@
+package force
+
+import (
+	"errors"
+	"github.com/emberfarkas/goctl/internal/migrate/clap"
+	"github.com/go-bamboo/pkg/log"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/spf13/cobra"
+)
+
+var (
+	sourceURL   string
+	path        string
+	databaseURL string
+	version     int
+)
+
+var Cmd = &cobra.Command{
+	Use:   "force",
+	Short: "后退一步数据库",
+	Long:  `后退数据库`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var cmdParse clap.YamlConfig
+		if clap.GenPath != "" {
+			configFileParams, err := clap.LoadConfigFile(clap.GenPath)
+			if err != nil {
+				return err
+			}
+			cmdParse = *configFileParams
+		} else {
+			// cmd first
+			if sourceURL != "" {
+				cmdParse.SourceURL = sourceURL
+			}
+			if sourceURL == "" && path != "" {
+				cmdParse.SourceURL = path
+			}
+			if databaseURL != "" {
+				cmdParse.DatabaseURL = databaseURL
+			}
+		}
+		m, err := migrate.New(cmdParse.SourceURL, cmdParse.DatabaseURL)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if _, err := m.Close(); err != nil {
+				log.Error(err)
+			}
+		}()
+		if err = m.Force(version); err != nil {
+			if errors.Is(err, migrate.ErrNoChange) {
+				return nil
+			}
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	Cmd.Flags().StringVar(&sourceURL, "source", "file://./migrations", "file://./migrations")
+	Cmd.Flags().StringVar(&path, "path", "./migrations", "specify a directory for output")
+	Cmd.Flags().StringVar(&databaseURL, "db", "", "input mysql or postgres or sqlite or sqlserver. consult[https://gorm.io/docs/connecting_to_the_database.html]")
+	Cmd.Flags().IntVar(&version, "version", -1, "fix and force version")
+}
--- internal/migrate/migrate.go
+++ internal/migrate/migrate.go
@@ -3,6 +3,7 @@ package migrate
 import (
 	"github.com/emberfarkas/goctl/internal/migrate/clap"
 	"github.com/emberfarkas/goctl/internal/migrate/down"
+	"github.com/emberfarkas/goctl/internal/migrate/force"
 	"github.com/emberfarkas/goctl/internal/migrate/new"
 	"github.com/emberfarkas/goctl/internal/migrate/up"
 
@@ -25,6 +26,6 @@ var Cmd = &cobra.Command{
 }
 
 func init() {
-	Cmd.AddCommand(new.Cmd, up.Cmd, down.Cmd)
+	Cmd.AddCommand(new.Cmd, up.Cmd, down.Cmd, force.Cmd)
 	Cmd.PersistentFlags().StringVarP(&clap.GenPath, "conf", "c", "", "is path for gen.yml")
 }
